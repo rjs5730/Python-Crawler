6d93aa21977ffd4d4d5735181f96bbd49a1b53cc
Dev <rjs5730@naver.com>
2018-05-16 00:03:32 +0900

    hotfix

--- my_quote.py
+++ my_quote.py
@@ -11,8 +11,8 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract(),
+                'author': quote.css('small.author::text').extract(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
   
